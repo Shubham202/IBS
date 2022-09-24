c69b8891b3d9a00f71993adbe4a5aa2f5e1a0a54
Shubham202 <51954852+Shubham202@users.noreply.github.com>
2022-09-25 00:22:42 +0530

    modified:   html/support.html 	new file:   scripts/support.js 	modified:   stylesheets/support.css

---
+++ scripts/support.js
@@ -0,0 +1,39 @@
+$(".feedform2").hide();
+$(".feedform3").hide();
+
+$(".complaint").click(function () {
+    $(".feedform2").hide();
+    $(".feedform3").hide();
+    $(".feedform1").slideDown("slow");
+});
+
+$(".csubmit").click(function () {
+    alert(
+        "Your complaint has been filed and will be looked into \n Thank you and sorry for the inconvience!"
+    );
+    $("feedform1").hide();
+});
+
+$(".feedback").click(function () {
+    $(".feedform1").hide();
+    $(".feedform3").hide();
+    $(".feedform2").slideDown("slow");
+});
+
+$(".fsubmit").click(function () {
+    alert("Your feedback has been resgistered \n Thank you!");
+    $("feedform2").hide();
+});
+
+$(".suggestion").click(function () {
+    $(".feedform2").hide();
+    $(".feedform1").hide();
+    $(".feedform3").slideDown("slow");
+});
+
+$(".ssubmit").click(function () {
+    alert(
+        "Your suggestion has been registered and will be looked into \n Thank you!"
+    );
+    $("feedform3").hide();
+});
